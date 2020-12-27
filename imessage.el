a3a7975c3ffce647c5e2167e7e589476bc462678
Ian Wahbe <ian@wahbe.com>
2020-12-26 18:37:35 -0800

    IMessage.el mostly can display what I want it to

--- imessage.el
+++ imessage.el
@@ -37,49 +37,109 @@
                (progn
                  (concat "\nWHERE chat.chat_identifier = '" identifier "'")))
            "\nORDER BY
-    message_date DESC\nLIMIT\n    "
+    message_date DESC
+    LIMIT "
            (number-to-string limit)
            ";")
    buffer))
+
+(cl-defun imessage-chat-identifiers-query (buffer &key (limit 10))
+  (imessage-query
+   (concat "SELECT DISTINCT
+        chat.chat_identifier
+    FROM
+        chat
+    JOIN chat_message_join ON chat. \"ROWID\" = chat_message_join.chat_id
+    JOIN message ON chat_message_join.message_id = message. \"ROWID\"
+    ORDER BY
+        message_date ASC
+    LIMIT "
+           (number-to-string limit)
+           ";")
+   buffer))
+
+(defun imessage-chat-identifiers ()
+  "Returns a list of valid chat identifiers, and nil"
+  (with-temp-buffer
+    (imessage-chat-identifiers-query (current-buffer) :limit 1000)
+    (mapcar (lambda (x) (elt x 0)) (imessage-read-csv (current-buffer) 1))))
+
+(defun imessage-select-conversation (&optional conversation)
+  "Selects a conversation to focus on."
+  (interactive)
+  (when conversation (cl-assert (stringp conversation)))
+  (setq imessage-conversation (or conversation
+                                  (completing-read "Chat: " (cons nil (imessage-chat-identifiers)) nil t)))
+  (imessage-refresh))
+
+(defun imessage-refresh ()
+  "Refreshes the contents of an imessage-mode buffer."
+  (interactive)
+  (setq tabulated-list-entries (imessage-make-entries))
+  (tabulated-list-revert))
 
 (define-derived-mode imessage-mode tabulated-list-mode
   (setq tabulated-list-format
         [("me" 3 t)
          ("date" 20 t)
          ("chat" 25 t)
-         ("text" 1 nil)]))
+         ("text" 1 nil)])
+  (tabulated-list-init-header))
+
+(defun imessage-read-csv (buffer ncolumns)
+  "Reads a CSV with NCOLUMNS in BUFFER."
+  (cl-assert (> ncolumns 0))
+  (let (res)
+    (with-current-buffer buffer
+      (goto-char (point-min))
+      (while (not (eobp))
+        (setq res (cons
+                   (eval `(vector ,@(mapcar 'funcall
+                                            (make-list ncolumns 'imessage-read-csv-field))))
+                   res)))
+      (reverse res))))
+
+(defun imessage-read-csv-field ()
+  "Parses a csv field starting at point in the current buffer.
+Fields are seperated by commas or new lines. If A field contains either, it must be quoted.
+Quotes are escaped by quotes. We handle this with a finite state machine.
+
+NO-ESCAPE != \" -> NO-ESCAPE | == \" -> maybe-done
+MAYBE-DONE == \" -> NO-ESCAPE | == \" -> DONE
+"
+  (string-replace "\n" "\\n" (if (= (char-after) ?\")
+                                 (let ((start (1+ (point)))
+                                       maybe-escaped done)
+                                   (forward-char)
+                                   (while (not done)
+                                     (cond
+                                      ((and maybe-escaped (= (char-after) ?\"))
+                                       (setq maybe-escaped nil))
+                                      ;; We know that the next char is not ", so we esacape.
+                                      (maybe-escaped (setq done t))
+                                      ;; We know that maybe-escaped is false, and we hit a ", so we set it.
+                                      ((= (char-after) ?\")
+                                       (setq maybe-escaped t)))
+                                     (forward-char))
+                                   (buffer-substring-no-properties start (- (point) 2)))
+                               (let ((start (point)))
+                                 (re-search-forward "[,\n]")
+                                 (buffer-substring-no-properties start (1- (point)))))))
 
 (defun imessage-make-entries ()
-  (let (res
-        (next (lambda nil
-                (let (r)
-                  (setq r (read (current-buffer)))
-                  (forward-char)
-                  (if (numberp r)
-                      (number-to-string r) r)))))
+  "Generates entries suitable for `tabulated-list-entries'"
+  (let ((query-ident imessage-conversation))
     (with-temp-buffer
-      (imessage-chat-query
-       (current-buffer)
-       :limit
-       (frame-height)
-       :identifier imessage-conversation)
-      (goto-char
-       (point-min))
-      (while
-          (not
-           (eobp))
-        (setq res
-              (cons
-               (vector
-                (funcall next) (funcall next)
-                (funcall next) (funcall next))
-               res))
-        (forward-line 1))
-      (reverse res))))
+      (imessage-chat-query (current-buffer)
+                           :limit (* 2 (frame-height))
+                           :identifier query-ident)
+      (mapcar (lambda (e) (list nil e)) (imessage-read-csv (current-buffer) 4)))))
 
 
 (defun imessage ()
+  "Sets up imessage."
   (interactive)
-  (switch-to-buffer (get-buffer-create "*imessage*"))
+  (switch-to-buffer (get-buffer-create "*imessage*" t) t)
   (setq tabulated-list-entries (imessage-make-entries))
-  (imessage-mode))
+  (imessage-mode)
+  (tabulated-list-print))
